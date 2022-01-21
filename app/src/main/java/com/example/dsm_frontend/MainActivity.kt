c47a1117fa5df47f7af2c3c64374ffc3fbd9f960
AlvaroHJ21 <alvarohuaysara@gmail.com>
2022-01-21 15:35:17 -0500

    bottom nav v1

--- app/src/main/java/com/example/dsm_frontend/MainActivity.kt
+++ app/src/main/java/com/example/dsm_frontend/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.dsm_frontend
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.dsm_frontend.carritoModule.MainCarFragment
+import com.example.dsm_frontend.databinding.ActivityMainBinding
+import com.example.dsm_frontend.searchModule.search.MainSearchFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        val navView = mBinding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        navView.setupWithNavController(navController)
+
     }
 }
---
+++ app/src/main/java/com/example/dsm_frontend/carritoModule/MainCarFragment.kt
@@ -0,0 +1,14 @@
+package com.example.dsm_frontend.carritoModule
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.dsm_frontend.R
+
+class MainCarFragment : Fragment(R.layout.fragment_main_car) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
