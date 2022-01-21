2ddea7ae660f5bfa14aaaee430d79854e4938af0
AlvaroHJ21 <alvarohuaysara@gmail.com>
2022-01-21 11:32:46 -0500

    T - 43 Frontend - Interfaz para visualizar tiendas cercanas

--- app/src/main/java/com/example/dsm_frontend/searchedModule/search/StoresFragment.kt
+++ app/src/main/java/com/example/dsm_frontend/searchedModule/search/StoresFragment.kt
@@ -5,11 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.View
 import com.example.dsm_frontend.R
 import com.example.dsm_frontend.databinding.FragmentStoresBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class StoresFragment : Fragment(R.layout.fragment_stores)/*, OnMapReadyCallback*/ {
+class StoresFragment : Fragment(R.layout.fragment_stores), OnMapReadyCallback {
 
     private lateinit var mBinding: FragmentStoresBinding
-    /*private lateinit var mMap: GoogleMap*/
+    private lateinit var mMap: GoogleMap
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -17,8 +23,8 @@ class StoresFragment : Fragment(R.layout.fragment_stores)/*, OnMapReadyCallback*
 
         setupComponents()
 
-        /*val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)*/
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
 
     }
@@ -26,12 +32,12 @@ class StoresFragment : Fragment(R.layout.fragment_stores)/*, OnMapReadyCallback*
     private fun setupComponents() {
     }
 
-    /*override fun onMapReady(googleMap: GoogleMap) {
+    override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-    }*/
+        val ubi = LatLng(-12.0572215,-77.0854449)
+        mMap.addMarker(MarkerOptions().position(ubi).title("UNMSM"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ubi))
+    }
 }
