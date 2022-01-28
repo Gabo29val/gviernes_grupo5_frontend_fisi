efcfac42c0f59eea19b0033c9b6ea86a323e26ab
leav1998 <luis.avila@unmsm.edu.pe>
2022-01-27 23:57:28 -0500

    "pasarela epica de pago"

--- app/src/main/java/com/example/dsm_frontend/carritoModule/carrito/MainCarFragment.kt
+++ app/src/main/java/com/example/dsm_frontend/carritoModule/carrito/MainCarFragment.kt
@@ -1,8 +1,20 @@
 package com.example.dsm_frontend.carritoModule.carrito
 
+import android.util.Log
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.stripe.android.PaymentConfiguration
+import com.stripe.android.paymentsheet.PaymentSheet
+import com.stripe.android.paymentsheet.PaymentSheetResult
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.IOException
+import androidx.fragment.app.Fragment
 import com.example.dsm_frontend.R
 import com.example.dsm_frontend.carritoModule.carrito.adapter.ProductCarAdapter
 import com.example.dsm_frontend.databinding.FragmentMainCarBinding
@@ -24,6 +36,28 @@ class MainCarFragment : Fragment(R.layout.fragment_main_car) {
             adapter = mProductAdapter
             setHasFixedSize(true)
         }
+        val payment : Payment =Payment(mBinding.tvAmountToPay.text.toString(),mBinding.btnPay, requireContext())
+        mBinding.btnPay.setOnClickListener{
+            println("Pagando ...")
+            requireActivity().runOnUiThread {
+                payment!!.onPayClicked(view)
+            }
+        }
+        PaymentConfiguration.init(
+            requireContext(),
+            "pk_test_51KM3QZG6zQNEntYYBTviK5vbbI0sloSqUoJ5ZFcpcjkApfD83KE53soTFIBYqZDWNqfRnGJvOvQmw3AmVRVojCPq00XmTq9dLN"
+        );
+        //mBinding.btnPay.setEnabled(false)
+        requireActivity().runOnUiThread {
+            payment!!.paymentSheet = PaymentSheet(
+                this
+            ) { paymentSheetResult: PaymentSheetResult ->
+                payment!!.onPaymentSheetResult(
+                    paymentSheetResult
+                )
+            }
+        }
+        requireActivity().runOnUiThread {    payment!!.fetchPaymentIntent()}
     }
 
     private fun getProducts(): List<Product> {
---
+++ app/src/main/java/com/example/dsm_frontend/carritoModule/carrito/Payment.kt
@@ -0,0 +1,112 @@
+package com.example.dsm_frontend.carritoModule.carrito
+
+import android.app.Activity
+import android.content.Context
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.stripe.android.paymentsheet.PaymentSheet
+import com.stripe.android.paymentsheet.PaymentSheetResult
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.IOException
+
+class Payment (var amount:String?="", var payButton:Button, var context:Context){
+     val TAG = "CheckoutActivity"
+     val BACKEND_URL = "http://10.0.2.2:4242"
+
+     var paymentIntentClientSecret: String? = null
+     var paymentSheet: PaymentSheet? = null
+
+     fun showAlert(title: String, message: String?) {
+            val dialog =
+                AlertDialog.Builder(context)
+                    .setTitle(title)
+                    .setMessage(message)
+                    .setPositiveButton("Ok", null)
+                    .create()
+            dialog.show()
+
+    }
+
+     fun showToast(message: String) {
+
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+    }
+
+     fun fetchPaymentIntent() {
+        //val shoppingCartContent = "{\"id\":  ${binding.editText.text.toString()}}"
+        val shoppingCartContent = "{\"id\":  ${this.amount}}"
+
+        val requestBody: RequestBody = RequestBody.create(
+            "application/json; charset=utf-8".toMediaType(),shoppingCartContent
+        )
+        val request: Request = Request.Builder()
+            .url("$BACKEND_URL/create-payment-intent")
+            .post(requestBody)
+            .build()
+        OkHttpClient()
+            .newCall(request)
+            .enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    showAlert("Failed to load data", "Error: $e")
+                }
+
+                @Throws(IOException::class)
+                override fun onResponse(
+                    call: Call,
+                    response: Response
+                ) {
+                    if (!response.isSuccessful) {
+                        showAlert(
+                            "Failed to load page",
+                            "Error: $response"
+                        )
+
+                    } else {
+                        val responseJson = parseResponse(response.body)
+                        paymentIntentClientSecret = responseJson.optString("clientSecret")
+                        //payButton!!.isEnabled = true
+                        Log.i(TAG, "Retrieved PaymentIntent")
+                    }
+                }
+            })
+    }
+
+     fun parseResponse(responseBody: ResponseBody?): JSONObject {
+        if (responseBody != null) {
+            try {
+                return JSONObject(responseBody.string())
+            } catch (e: IOException) {
+                Log.e(TAG, "Error parsing response", e)
+            } catch (e: JSONException) {
+                Log.e(TAG, "Error parsing response", e)
+            }
+        }
+        return JSONObject()
+    }
+
+     fun onPayClicked(view: View) {
+        val configuration = PaymentSheet.Configuration("Example, Inc.")
+
+        // Present Payment Sheet
+        paymentSheet!!.presentWithPaymentIntent(paymentIntentClientSecret!!, configuration)
+    }
+
+     fun onPaymentSheetResult(
+        paymentSheetResult: PaymentSheetResult
+    ) {
+        if (paymentSheetResult is PaymentSheetResult.Completed) {
+            showToast("Payment complete!")
+        } else if (paymentSheetResult is PaymentSheetResult.Canceled) {
+            Log.i(TAG, "Payment canceled!")
+        } else if (paymentSheetResult is PaymentSheetResult.Failed) {
+            val error: Throwable = (paymentSheetResult as PaymentSheetResult.Failed).error
+            showAlert("Payment failed", error.localizedMessage)
+        }
+    }
+}
